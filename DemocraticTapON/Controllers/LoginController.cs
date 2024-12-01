9714c6ed45d6162e9d199c6c014294dbdd05d479
Silence_People <lamichhanesubash26@gmail.com>
2024-12-01 17:15:33 +1100

    Make a basic login page

---
+++ DemocraticTapON/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using DemocraticTapON.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemocraticTapON.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Implement your login logic here
+                // e.g., check the username and password against a database or authentication service
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
